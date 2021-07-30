41986dae82294afbdad56b9b217d52743cea3208
Dietrich Rordorf <rordorf@mdpi.com>
2021-07-30 22:00:46 +0200

    add certificate sentence to landing page

--- resources/views/app/start/index.blade.php
+++ resources/views/app/start/index.blade.php
@@ -15,12 +15,15 @@
                     Launchlabs (Gundeldingerfeld), Basel
                 </h2>
                 <div class="block">
-                    <a href="https://eventfrog.ch/de/events.html?searchTerm=baselhack" target="_blank" class="inline-block mb-8 underline">
-                        Register yout ticket
+                    <a href="https://eventfrog.ch/de/events.html?searchTerm=baselhack" target="_blank" class="inline-block mb-2 underline">
+                        Register your ticket
                     </a> |
-                    <a href="/blog/registrations-open-for-baselhack-2021" class="inline-block mb-8 underline">
+                    <a href="/blog/registrations-open-for-baselhack-2021" class="inline-block mb-2 underline">
                         More information
                     </a>
+                </div>
+                <div class="block mb-8">
+                    Entry only with a valid Swiss or EU COVID certificate.
                 </div>
                 <h2 class="text-xl tracking-tight text-gray-900 sm:text-xl md:text-2xl">
                     <span class="block xl:inline">We are more than a Hackathon. We connect people, take fun seriously and grow open innovation.</span>
