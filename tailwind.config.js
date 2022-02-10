342e50cfbf4995869ffc6f10e0cefaaab0b6f33e
rb <r.brand@iwf.ch>
2022-02-10 11:39:20 +0100

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js'
+    ],
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            'sans': ['hack', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+        },
+        extend: {},
+        colors: {
+            gray: colors.trueGray,
+            black: colors.black,
+            white: colors.white
+        }
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
