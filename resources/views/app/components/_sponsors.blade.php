90a04cbccece98956b9f588b95f7deba61d1076e
rb <r.brand@iwf.ch>
2022-07-15 08:43:52 +0200

    sponsoring page facelifting

--- resources/views/app/components/_sponsors.blade.php
+++ resources/views/app/components/_sponsors.blade.php
@@ -5,7 +5,7 @@
         Main Sponsors
     </h3>
 
-    <x-sponsors height=36 gridClass="grid grid-cols-1 gap-0.5 md:grid-cols-2 mt-4 lg:mt-8" :sponsors="$mainSponsors" />
+    <x-sponsors height=48 gridClass="grid grid-cols-2 gap-0.5 md:grid-cols-2 mt-4 lg:mt-8" :sponsors="$mainSponsors" />
 
 @endif
 
@@ -15,7 +15,7 @@
         Gold Sponsors
     </h3>
 
-    <x-sponsors height=24 gridClass="grid grid-cols-1 gap-0.5 md:grid-cols-3 mt-4 lg:mt-8" :sponsors="$goldSponsors" />
+    <x-sponsors height=36 gridClass="grid grid-cols-2 gap-0.5 md:grid-cols-3 mt-4 lg:mt-8" :sponsors="$goldSponsors" />
 
 @endif
 
@@ -25,7 +25,7 @@
         Silver Sponsors
     </h3>
 
-    <x-sponsors height=12 gridClass="grid grid-cols-2 gap-0.5 md:grid-cols-4 mt-4 lg:mt-8" :sponsors="$sponsors" />
+    <x-sponsors height=24 gridClass="grid grid-cols-2 gap-0.5 md:grid-cols-4 mt-4 lg:mt-8" :sponsors="$sponsors" />
 
 @endif
 
@@ -35,6 +35,6 @@
         Location Sponsor
     </h3>
 
-    <x-sponsors height=36 gridClass="grid grid-cols-2 gap-0.5 md:grid-cols-1 mt-4 lg:mt-8" :sponsors="$locationSponsors" />
+    <x-sponsors height=52 gridClass="grid grid-cols-2 gap-0.5 md:grid-cols-1 mt-4 lg:mt-8" :sponsors="$locationSponsors" />
 
 @endif
