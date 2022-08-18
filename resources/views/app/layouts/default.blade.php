82322d000414580cf780f9112b75e88c98eb0309
Dietrich Rordorf <rordorf@mdpi.com>
2022-08-18 10:32:16 +0200

    add Google Analytics tracking

--- resources/views/app/layouts/default.blade.php
+++ resources/views/app/layouts/default.blade.php
@@ -32,7 +32,16 @@
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 @if(app()->environment('production'))
+    <!-- Codebar tracker -->
     <script src="https://dragonfly.codebar.ch/script.js" site="VIJIXWCP" defer></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QGVQJQ0T9J"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-QGVQJQ0T9J');
+    </script>
 @endif
 
 @yield('scripts')
