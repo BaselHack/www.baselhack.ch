8e9ef5c0fdf2d5059b8662b67560673a666b218f
Dietrich Rordorf <rordorf@mdpi.com>
2022-08-19 15:10:22 +0200

    sponsor labelling

--- resources/views/app/components/_sponsors.blade.php
+++ resources/views/app/components/_sponsors.blade.php
@@ -1,28 +1,44 @@
 <h2 class="text-xl text-white font-medium mb-12 mt-24 text-center">Sponsors 2022</h2>
 @if(!empty($mainSponsors) && $mainSponsors->count())
     <h3 class="text-base text-white font-medium mb-4 mt-8 text-center">
-        Main Sponsors
+        @if($mainSponsors->count() > 1)
+            Main Sponsors
+        @else
+            Main Sponsor
+        @endif
     </h3>
     <x-sponsors height=48 gridClass="grid-cols-1 md:grid-cols-2" :sponsors="$mainSponsors" />
 @endif
 
 @if(!empty($goldSponsors) && $goldSponsors->count())
     <h3 class="text-base text-white font-medium mb-4 mt-8 text-center">
-        Gold Sponsors
+        @if($goldSponsors->count() > 1)
+            Gold Sponsors
+        @else
+            Gold Sponsor
+        @endif
     </h3>
     <x-sponsors height=36 gridClass="grid-cols-2 md:grid-cols-3" :sponsors="$goldSponsors" />
 @endif
 
 @if(!empty($silverSponsors) && $silverSponsors->count())
     <h3 class="text-base text-white font-medium mb-4 mt-8 text-center">
-        Silver Sponsors
+        @if($silverSponsors->count() > 1)
+            Silver Sponsors
+        @else
+            Silver Sponsor
+        @endif
     </h3>
     <x-sponsors height=24 gridClass="grid-cols-3 md:grid-cols-5" :sponsors="$silverSponsors" />
 @endif
 
 @if(!empty($locationSponsors) && $locationSponsors->count())
     <h3 class="text-base text-white font-medium mb-4 mt-8 text-center">
-        Location Sponsor
+        @if($locationSponsors->count() > 1)
+            Location Sponsors
+        @else
+            Location Sponsor
+        @endif
     </h3>
     <x-sponsors height=52 gridClass="grid-cols-1 md:grid-cols-2" :sponsors="$locationSponsors" />
 @endif
