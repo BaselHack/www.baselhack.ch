e1a6f2157e1f94343ed07e813ce919f09571c59d
Dietrich Rordorf <rordorf@mdpi.com>
2022-10-06 11:53:30 +0200

    add previous annual reports from github

--- resources/views/app/about/index.blade.php
+++ resources/views/app/about/index.blade.php
@@ -15,7 +15,29 @@
                 <h2 class="text-xl font-bold mb-8">Annual Reports</h2>
                 <ul>
                     <li>
-                        <a href="/pdfs/baselhack-jahresbericht-2021.pdf">Annual Report 2021 (in German, PDF, 9.6 MB)</a>
+                        <a href="/pdfs/baselhack-jahresbericht-2021.pdf">
+                            Annual Report 2021 (in German, PDF)
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/BaselHack/Verein/wiki/Mitgliederversammlung-BaselHack-2021#jahresbericht" target="_blank">
+                            Annual Report 2020 (in German, on GitHub)
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/BaselHack/Verein/wiki/Mitgliederversammlung-BaselHack-2020#jahresbericht" target="_blank">
+                            Annual Report 2019 (in German, on GitHub)
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/BaselHack/Verein/wiki/Mitgliederversammlung-BaselHack-2019#jahresbericht" target="_blank">
+                            Annual Report 2018 (in German, on GitHub)
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/BaselHack/Verein/wiki/Mitgliederversammlung-BaselHack-2018#jahresbericht" target="_blank">
+                            Annual Report 2017 (in German, on GitHub)
+                        </a>
                     </li>
                 </ul>
             </div>
