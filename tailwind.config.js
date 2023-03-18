66d89c35c3e27ffa1f6ea60c452782b3dd2c5c79
Sebastian Fix <sebastian.fix@codebar.ch>
2023-03-18 14:48:59 +0100

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,42 @@
-const colors = require("tailwindcss/colors");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: [
+    content: [
         './resources/**/*.blade.php',
-        './resources/**/*.js'
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     ],
-    darkMode: false,
     theme: {
-        fontFamily: {
-            'sans': ['Inconsolata', 'monospace']
-        },
-        fontSize: {
-            'sm': '0.88889rem',     // 16px - small
-            'base': '1rem',         // 18px - p, default
-            'xl': '1.33333rem',     // 24px - h3
-            '2xl': '1.55556em',     // 28px - h2
-            '3xl': '1.77778rm',     // 32px - h1
-            '4xl': '2em',           // 36px
-            '5xl': '2.22222rem',    // 40px
-        },
-        extend: {},
-        colors: {
-            gray: colors.trueGray,
-            black: colors.black,
-            white: colors.white,
-            yellow: {
-                DEFAULT: '#f2e94d',
-                'accent': '#d4ca17',
+        extend: {
+            fontFamily: {
+                'sans': ['Inconsolata', 'monospace']
             },
-            teal: {
-                DEFAULT: '#5ebdc9',
-                'accent': '#3298a6',
+            fontSize: {
+                'sm': '0.88889rem',     // 16px - small
+                'base': '1rem',         // 18px - p, default
+                'xl': '1.33333rem',     // 24px - h3
+                '2xl': '1.55556em',     // 28px - h2
+                '3xl': '1.77778rm',     // 32px - h1
+                '4xl': '2em',           // 36px
+                '5xl': '2.22222rem',    // 40px
+            },
+            colors: {
+                gray: colors.trueGray,
+                black: colors.black,
+                white: colors.white,
+                yellow: {
+                    DEFAULT: '#f2e94d',
+                    'accent': '#d4ca17',
+                },
+                teal: {
+                    DEFAULT: '#5ebdc9',
+                    'accent': '#3298a6',
+                }
             }
-        }
+        },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-}
+
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
