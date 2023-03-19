2684325170e6d2efad9e2ee6d96248da7816c7b8
Sebastian Fix <sebastian.fix@codebar.ch>
2023-03-19 11:27:26 +0100

    WIP

--- config/lasso.php
+++ config/lasso.php
@@ -9,7 +9,7 @@ return [
          * phase. This will most likely be "npm run production" but
          * you may choose what you would like to execute.
          */
-        'script' => 'npm run production',
+        'script' => 'npm run build',
 
         /*
          * Configure the amount of time (in seconds) the compiler
