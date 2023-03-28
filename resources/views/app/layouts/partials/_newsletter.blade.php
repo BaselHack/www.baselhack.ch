ec6ccd2bd697a45c8ed74730499ddb1d693a6a73
Christian Huber <c.huber@iwf.ch>
2023-03-28 21:08:44 +0200

    content updates, ddev port changes (80 / 443), temp. disable unused/outdated navpoints

--- resources/views/app/layouts/partials/_newsletter.blade.php
+++ resources/views/app/layouts/partials/_newsletter.blade.php
@@ -1,12 +1,12 @@
 
-<div class="bg-black text-white">
-    <div class="max-w-screen-xl mx-auto px-4 py-12 sm:px-6 lg:py-16 lg:px-8">
-        <div class="px-6 py-6 border-2 border-white-600 rounded-lg md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center">
+<div class="text-white bg-black">
+    <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8">
+        <div class="px-6 py-6 border-2 rounded-lg border-white-600 md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center">
             <div class="xl:w-0 xl:flex-1">
-                <h2 class="text-2xl leading-8 font-extrabold tracking-tight text-white sm:text-3xl sm:leading-9">
+                <h2 class="text-2xl font-extrabold leading-8 tracking-tight text-white sm:text-3xl sm:leading-9">
                     {{ __('app/newsletter.title') }}
                 </h2>
-                <p class="mt-3 max-w-3xl text-lg leading-6 text-gray-300" id="newsletter-headline">
+                <p class="max-w-3xl mt-3 text-lg leading-6 text-gray-300" id="newsletter-headline">
                     {{ __('app/newsletter.subtitle') }}
                 </p>
             </div>
@@ -14,9 +14,9 @@
                 <form class="sm:flex" aria-labelledby="newsletter-headline" method="POST" action="{{ route('newsletter.store') }}">
                     @csrf
                     @honeypot
-                    <input id="email" name="email" aria-label="{{ __('app/newsletter.form.input.email') }}" type="email" required class="appearance-none w-full px-5 py-3 border border-transparent text-base leading-6 rounded-md text-white bg-black text-white placeholder-gray-200 focus:outline-none focus:placeholder-gray-200 transition duration-150 ease-in-out" placeholder="{{ __('app/newsletter.form.input.email') }}">
+                    <input id="email" name="email" aria-label="{{ __('app/newsletter.form.input.email') }}" type="email" required class="w-full px-5 py-3 text-base leading-6 text-white placeholder-gray-200 transition duration-150 ease-in-out bg-black border border-white rounded-md appearance-none focus:outline-none focus:placeholder-gray-200" placeholder="{{ __('app/newsletter.form.input.email') }}">
                     <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
-                        <button class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gray-600 hover:bg-gray-400 focus:outline-none focus:bg-gray-400 transition duration-150 ease-in-out">
+                        <button class="flex items-center justify-center w-full px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-gray-600 border border-transparent rounded-md hover:bg-gray-400 focus:outline-none focus:bg-gray-400">
                             {{ __('app/newsletter.form.button') }}
                         </button>
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -39,5 +39,9 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require('@tailwindcss/forms'), 
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio')
+    ],
 };
