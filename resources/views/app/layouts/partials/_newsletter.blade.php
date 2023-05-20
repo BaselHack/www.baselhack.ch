602a194a33b7a802961315065f2fb27ee5f87382
Christian Huber <corethereal@gmail.com>
2023-05-20 23:58:11 +0200

    styling fixes + button layout improvements

--- resources/views/app/layouts/partials/_newsletter.blade.php
+++ resources/views/app/layouts/partials/_newsletter.blade.php
@@ -1,5 +1,5 @@
 <div class="container px-8 mx-auto">
-    <div class="max-w-6xl p-12 mx-auto my-8 border-2 rounded-lg md:my-16 newsletter-signup border-slate-300">
+    <div class="max-w-6xl p-8 mx-auto my-8 border-2 rounded-lg md:p-12 md:my-16 newsletter-signup border-slate-300">
         <div class="flex grid items-center grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-12">
             <div class="mb-4 lg:mb-0">
                 <h2 class="flex items-center gap-4 mb-4 text-2xl font-bold">
@@ -14,8 +14,8 @@
                 <form class="flex flex-col flex-wrap justify-start w-full gap-3 sm:flex-row sm:w-auto" aria-labelledby="newsletter-headline" method="POST" action="{{ route('newsletter.store') }}">
                     @csrf
                     @honeypot
-                    <input id="email" name="email" aria-label="{{ __('app/newsletter.form.input.email') }}" type="email" required class="w-full px-4 py-3 bg-transparent border-2 border-white rounded-md sm:w-80" placeholder="{{ __('app/newsletter.form.input.email') }}">
-                    <input type="submit" class="px-4 py-2 rounded-md cursor-pointer bg-gradient-to-tr hover:from-yellow-500 transition-colors hover:to-yellow-500 from-[#efe73e] to-[#50b1bf] text-black" value="{{ __('app/newsletter.form.button') }}">
+                    <input id="email" name="email" aria-label="{{ __('app/newsletter.form.input.email') }}" type="email" required class="w-full px-4 py-3 bg-transparent border-2 border-white rounded-md focus:border-yellow-500 focus:ring-transparent sm:w-80" placeholder="{{ __('app/newsletter.form.input.email') }}">
+                    <input type="submit" class="hover:from-yellow-500 hover:to-yellow-500 focus:outline outline-offset-2 outline-2 outline-yellow-500 w-full md:w-auto px-4 py-2 rounded-md cursor-pointer bg-gradient-to-tr transition-colors from-[#efe73e] to-[#50b1bf] text-black focus:from-yellow-500 focus:to-yellow-500" value="{{ __('app/newsletter.form.button') }}">
                 </form>
                 @error('email')
                     <p class="mt-3 text-sm leading-5 text-red-200">
--- resources/views/app/start/index.blade.php
+++ resources/views/app/start/index.blade.php
@@ -32,7 +32,7 @@
                 </div>
 
 
-                <h2 class="max-w-screen-xl mt-24 text-4xl">About BaselHack</h2>
+                <h2 class="max-w-screen-xl mt-16 text-4xl md:mt-24">About BaselHack</h2>
                 <div class="flex justify-center mt-6">
                     <p class="max-w-2xl text-left">
                         BaselHack is the hackathon for Basel, Northwestern Switzerland and bordering Alsace and
@@ -52,14 +52,14 @@
                     <!--<a href="{{ asset('files/baselhack-2022.ics') }}"
                        class="inline-block px-6 py-2 m-2 text-sm font-medium text-white bg-gray-600 border border-transparent rounded-md md:text-base hover:bg-gray-400">
                         Add to Calendar</a>-->
-                    <a href="#more" class="w-full md:w-auto px-4 py-2 rounded-md cursor-pointer bg-gradient-to-tr hover:from-yellow-500 transition-colors hover:to-yellow-500 from-[#efe73e] to-[#50b1bf] text-black">Learn More</a>
-                    <a href="{{ route('posts.index') }}" class="w-full md:w-auto px-4 py-2 rounded-md cursor-pointer bg-gradient-to-tr hover:from-yellow-500 transition-colors hover:to-yellow-500 from-[#efe73e] to-[#50b1bf] text-black">Latest News</a>
+                    <a href="#more" class="focus:bg-yellow-500 focus:outline outline-offset-2 outline-2 outline-yellow-500 w-full md:w-auto px-4 py-2 rounded-md cursor-pointer bg-gradient-to-tr hover:from-yellow-500 transition-colors hover:to-yellow-500 from-[#efe73e] to-[#50b1bf] text-black">Learn More</a>
+                    <a href="{{ route('posts.index') }}" class="focus:bg-yellow-500 focus:outline outline-offset-2 outline-2 outline-yellow-500 w-full md:w-auto px-4 py-2 rounded-md cursor-pointer bg-gradient-to-tr hover:from-yellow-500 transition-colors hover:to-yellow-500 from-[#efe73e] to-[#50b1bf] text-black">Latest News</a>
                 </div>
             </div>
 
-            <div class="flex justify-center mt-40">
+            <div class="flex justify-center mt-20 md:mt-28">
                 <a id="more" name="more" class="mb-4"></a>
-                <h2 class="max-w-screen-xl mt-4 text-4xl">The BaselHack in a Nutshell</h2>
+                <h2 class="max-w-screen-xl mb-4 text-4xl">The BaselHack in a Nutshell</h2>
             </div>
 
             <div class="flex justify-center">
