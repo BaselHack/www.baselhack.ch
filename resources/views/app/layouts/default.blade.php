01ad7371115fd10c4491ef976648336cdc8913c6
Christian Huber <c.huber@iwf.ch>
2023-07-04 09:15:52 +0200

    add metatag for theme coloring -> iphone titlebar

--- resources/views/app/layouts/default.blade.php
+++ resources/views/app/layouts/default.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="theme-color" content="#161616">
 
     <!-- Fonts -->
     @include('app.layouts.partials._meta')
