cc6acd611f33130b06972edf754f115e8dccd023
Christian Huber <c.huber@iwf.ch>
2023-08-03 21:47:23 +0200

    responsiveness + wording

--- resources/views/app/components/_cta.blade.php
+++ resources/views/app/components/_cta.blade.php
@@ -32,7 +32,7 @@
                     <svg class="w-5 h-5 mt-1 shrink-0" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
                     </svg>
-                    <a class="underline text-start" href="https://www.letsboot.ch/basel-boot-2023" target="_blank">Bonus: Register for BaselBoot course by letsboot.ch for 90.-- CHF</a>
+                    <a class="underline text-start max-w-[28rem]" href="https://www.letsboot.ch/basel-boot-2023" target="_blank">Bonus: Register for BaselBoot co-event by letsboot.ch for 90.-- CHF</a>
                 </div>
                 <div class="flex flex-col items-center justify-end flex-1 mt-6 md:items-end md:mt-0">
                     <small class="inline-block mb-2">sponsored by:</small>
