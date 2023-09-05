fd3b3343b90b0ac280c298f706c1e23855673811
rb <r.brand@iwf.ch>
2023-09-05 18:21:40 +0200

    real logos for logo cloud, disabled empty pages in nav, removed sponsors block, changed a benefits title/text

--- resources/views/app/components/_benefits.blade.php
+++ resources/views/app/components/_benefits.blade.php
@@ -17,15 +17,15 @@
           <div class="flex flex-col bg-[#070707] p-6 rounded-sm">
             <dt class="text-lg font-semibold leading-7 text-white">
               <div class="flex items-center justify-center w-12 h-12 mb-6 text-black bg-yellow-500 rounded">
-                
+
                 <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-                </svg>                
+                </svg>
               </div>
-              Accomodation overnight possible
+              Bring your sleeping bag
             </dt>
             <dd class="flex flex-col flex-auto mt-1 text-base leading-7 text-gray-300">
-              <p class="flex-auto">No need to worry about rushing home. We've got you covered with overnight accommodation options.</p>
+              <p class="flex-auto">No need to worry about rushing home. We've got you covered with a place where you can sleep a few hours.</p>
             </dd>
           </div>
           <div class="flex flex-col bg-[#070707] p-6 rounded-sm">
@@ -33,7 +33,7 @@
               <div class="flex items-center justify-center w-12 h-12 mb-6 bg-yellow-500 rounded">
                 <svg class="w-8 h-8 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
-                </svg>                
+                </svg>
               </div>
               Exciting Challenges
             </dt>
@@ -46,7 +46,7 @@
               <div class="flex items-center justify-center w-12 h-12 mb-6 bg-yellow-500 rounded">
                 <svg class="w-8 h-8 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
-                </svg>                
+                </svg>
               </div>
               Vibrant Community
             </dt>
