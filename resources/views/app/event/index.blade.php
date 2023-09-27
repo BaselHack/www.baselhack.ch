aa8c3e558239035772b3874f712d39980255238a
Christian Huber <corethereal@gmail.com>
2023-09-28 00:56:26 +0200

    events page wip

--- resources/views/app/event/index.blade.php
+++ resources/views/app/event/index.blade.php
@@ -44,55 +44,63 @@
                     <div class="flex flex-col items-start justify-center p-8 text-white border bg-neutral-900 time-block border-neutral-800">
                         <div class="mb-6 text-xl font-bold leading-tight uppercase md:mb-8">Saturday, 27.10.2023</div>
                         <div class="mb-6 md:mb-8">
-                            <strong class="block text-lg font-normal">08:00 - 09:00</strong>
-                            <span class="font-light">Opening + Keynote</span>
+                            <strong class="block text-lg font-normal">09:00</strong>
+                            <span class="font-light">Door Opening</span>
                         </div>
                         <div class="mb-6 md:mb-8">
-                            <strong class="block text-lg font-normal">09:00 - 09:45</strong>
-                            <span class="font-light">Pitching & Challenges</span>
+                            <strong class="block text-lg font-normal">09:30 - 10:00</strong>
+                            <span class="font-light">Official program start in the Auditorium</span>
                         </div>
                         <div class="mb-6 md:mb-8">
-                            <strong class="block text-lg font-normal">09:45 - 12:00</strong>
-                            <span class="font-light">Start Hacking | Let's go!</span>
+                            <strong class="block text-lg font-normal">10:00 - 10:30</strong>
+                            <span class="font-light">Challenge Pitching | Pick a challenge or present your own</span>
+                        </div>
+                        <div class="mb-6 md:mb-8">
+                            <strong class="block text-lg font-normal">10.30 - 10:45</strong>
+                            <span class="font-light">Start work & coding in teams</span>
                         </div>
                         <div class="mb-6 md:mb-8">
                             <strong class="block text-lg font-normal">12:00 - 13:00</strong>
-                            <span class="font-light">Lunchbreak | Yum, nice food!</span>
+                            <span class="font-light">Lunchbreak</span>
                         </div>
                         <div class="mb-6 md:mb-8">
-                            <strong class="block text-lg font-normal">13:00 - 18:30</strong>
-                            <span class="font-light">Hacking</span>
+                            <strong class="block text-lg font-normal">13:00 - 16:00</strong>
+                            <span class="font-light">Hack | Let's goo!</span>
+                        </div>
+                        <div class="mb-6 text-neutral-400 md:mb-8">
+                            <strong class="block text-lg font-normal">16:30 - 17:00</strong>
+                            <span class="font-light">Voluntary Talk about FPV Drones</span>
                         </div>
                         <div>
-                            <strong class="block text-lg font-normal">18:30 - 19:30</strong>
-                            <span class="font-light">Hacking</span>
+                            <strong class="block text-lg font-normal">18:30</strong>
+                            <span class="font-light">Dinner</span>
                         </div>
                     </div>
                     <div class="flex flex-col items-start justify-center p-8 text-white border bg-neutral-900 time-block border-neutral-800">
                         <div class="mb-6 text-xl font-bold leading-tight uppercase md:mb-8">Sunday, 28.10.2023</div>
                         <div class="mb-6 md:mb-8">
-                            <strong class="block text-lg font-normal">08:00 - 09:00</strong>
-                            <span class="font-light">Opening + Keynote</span>
+                            <strong class="block text-lg font-normal">08:00</strong>
+                            <span class="font-light">Official Start of Day #2</span>
                         </div>
                         <div class="mb-6 md:mb-8">
-                            <strong class="block text-lg font-normal">09:00 - 09:45</strong>
-                            <span class="font-light">Pitching & Challenges</span>
+                            <strong class="block text-lg font-normal">12:00</strong>
+                            <span class="font-light">Lunchbreak</span>
+                        </div>
+                        <div class="mb-6 md:mb-8 text-neutral-400">
+                            <strong class="block text-lg font-normal">14:00 - 14:30</strong>
+                            <span class="font-light">Voluntary Talk | AI, the next big thing?</span>
                         </div>
                         <div class="mb-6 md:mb-8">
-                            <strong class="block text-lg font-normal">09:45 - 12:00</strong>
-                            <span class="font-light">Start Hacking | Let's go!</span>
+                            <strong class="block text-lg font-normal">16:00</strong>
+                            <span class="font-light">Hands off at exactly 16:00 for all teams!</span>
                         </div>
                         <div class="mb-6 md:mb-8">
-                            <strong class="block text-lg font-normal">12:00 - 13:00</strong>
-                            <span class="font-light">Lunchbreak | Yum, nice food!</span>
-                        </div>
-                        <div class="mb-6 md:mb-8">
-                            <strong class="block text-lg font-normal">13:00 - 18:30</strong>
-                            <span class="font-light">Hacking</span>
+                            <strong class="block text-lg font-normal">16:15</strong>
+                            <span class="font-light">Final Presentations to Jury and Crowd</span>
                         </div>
                         <div>
-                            <strong class="block text-lg font-normal">18:30 - 19:30</strong>
-                            <span class="font-light">Hacking</span>
+                            <strong class="block text-lg font-normal">17:30 - 18:00</strong>
+                            <span class="font-light">Awards and Closing</span>
                         </div>
                     </div>
                 </div>
@@ -150,7 +158,56 @@
             </div>
             <div class="my-12 md:my-24">
                 <h1 class="mb-5 text-2xl md:text-4xl">What to bring</h1>
-                <p>Bring a <strong>laptop</strong> and lots of energy, we got you covered on the rest!</p>
+                <p>
+                    Let's make sure you're armed and ready to conquer this adventure.<br>
+                    Here's your not-so-serious "What to bring" checklist to keep the vibes high and the fun flowing:
+                </p>
+                <ul role="list" class="grid grid-cols-1 gap-8 mt-8 md:mt-12 md:grid-cols-2">
+                    <li class="flex py-3 gap-x-6">
+                        <div class="flex flex-col gap-x-3">
+                            <p class="leading-6 uppercase"><strong>1. Laptop & Charger</strong></p>
+                            <p class="text-[1rem] text-neutral-300">Because you can't code with your mind (yet), you'll need this trusty sidekick. It's like your Excalibur in the digital realm.</p>
+                        </div>
+                    </li>
+                    <li class="flex py-3 gap-x-6">
+                        <div class="flex flex-col gap-x-3">
+                            <p class="leading-6 uppercase"><strong>2. Team Spirit</strong></p>
+                            <p class="text-[1rem] text-neutral-300">Don't forget to pack your A-game and team spirit. Enthusiasm is our secret weapon!</p>
+                        </div>
+                    </li>
+                    <li class="flex py-3 gap-x-6">
+                        <div class="flex flex-col gap-x-3">
+                            <p class="leading-6 uppercase"><strong>3. Rubber Ducks</strong></p>
+                            <p class="text-[1rem] text-neutral-300">Every coder's confidant. Use them to explain your code (they're great listeners) or as emergency stress balls.</p>
+                        </div>
+                    </li>
+                    <li class="flex py-3 gap-x-6">
+                        <div class="flex flex-col gap-x-3">
+                            <p class="leading-6 uppercase"><strong>4. Glow Sticks</strong></p>
+                            <p class="text-[1rem] text-neutral-300">Illuminate your genius ideas (literally) during those late-night brainstorming sessions.</p>
+                        </div>
+                    </li>
+                    <li class="flex py-3 gap-x-6">
+                        <div class="flex flex-col gap-x-3">
+                            <p class="leading-6 uppercase"><strong>5. Emoji Stickers</strong></p>
+                            <p class="text-[1rem] text-neutral-300">Decorate your laptop with emoji stickers to express your code's emotional journey.</p>
+                        </div>
+                    </li>
+                    <li class="flex py-3 gap-x-6">
+                        <div class="flex flex-col gap-x-3">
+                            <p class="leading-6 uppercase"><strong>6. Rubik's Cube</strong></p>
+                            <p class="text-[1rem] text-neutral-300">For those moments when your code isn't the only puzzle you want to solve.</p>
+                        </div>
+                    </li>
+                    <li class="flex py-3 gap-x-6">
+                        <div class="flex flex-col gap-x-3">
+                            <p class="leading-6 uppercase"><strong>7. A Sense of Humor</strong></p>
+                            <p class="text-[1rem] text-neutral-300">The most important item on the list! Keep it light, laugh often, and remember that hackathons are about fun and learning.</p>
+                        </div>
+                    </li>
+                </ul>
+                <p class="mt-8 prose">We forgot something? Hit us up on our Hackathon Slack Server: <a href="https://join.slack.com/t/baselhack-2023/" target="_blank">https://join.slack.com/t/baselhack-2023/</a></p>
+                <p class="mt-8">Remember, a hackathon is about more than just lines of code; it's about camaraderie, creativity, and having a blast. So, pack your essentials, gather your team, and let's code our way into the future while having a ton of fun along the way!</p>
             </div>
         </div>
     </main>
